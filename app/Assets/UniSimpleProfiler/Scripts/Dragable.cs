aec8801b6f33c40ddc3da2180e12cc613cfdbcdd
NickArthur <nickarthur.night@gmail.com>
2021-04-02 22:57:31 -0500

    added descriptive comments

--- app/Assets/UniSimpleProfiler/Scripts/Dragable.cs
+++ app/Assets/UniSimpleProfiler/Scripts/Dragable.cs
@@ -3,34 +3,39 @@ using UnityEngine.EventSystems;
 
 namespace UniSimpleProfiler.Internal
 {
-	/// <summary>
-	/// UI をドラッグできるようにするコンポーネント
-	/// </summary>
-	[DisallowMultipleComponent]
-	public sealed class Dragable : MonoBehaviour, IDragHandler
-	{
-		//====================================================================================
-		// 変数(SerializeField)
-		//====================================================================================
-		[SerializeField, HideInInspector] private RectTransform m_rectTransform = null;
+    /// <summary>
+    /// UI をドラッグできるようにするコンポーネント
+    /// A component that allows you to drag
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class Dragable : MonoBehaviour, IDragHandler
+    {
+        //====================================================================================
+        // 変数(SerializeField)
+        // variable
+        //====================================================================================
+        [SerializeField, HideInInspector]
+        private RectTransform m_rectTransform = null;
 
-		//====================================================================================
-		// 関数
-		//====================================================================================
-		/// <summary>
-		/// リセットされる時に呼び出されます
-		/// </summary>
-		private void Reset()
-		{
-			m_rectTransform = GetComponent<RectTransform>();
-		}
+        //====================================================================================
+        // 関数 function
+        //====================================================================================
+        /// <summary>
+        /// リセットされる時に呼び出されます
+        /// Called when reset
+        /// </summary>
+        private void Reset()
+        {
+            m_rectTransform = GetComponent<RectTransform>();
+        }
 
-		/// <summary>
-		/// ドラッグ中に呼び出されます
-		/// </summary>
-		public void OnDrag( PointerEventData e )
-		{
-			m_rectTransform.position += new Vector3( e.delta.x, e.delta.y, 0f );
-		}
-	}
+        /// <summary>
+        /// ドラッグ中に呼び出されます
+        /// Called while dragging
+        /// </summary>
+        public void OnDrag(PointerEventData e)
+        {
+            m_rectTransform.position += new Vector3(e.delta.x, e.delta.y, 0f);
+        }
+    }
 }
